1a10ebb1bf3bb99945f6768fb187a70195ced592
ppalladio <nine.slide@gmail.com>
2022-10-21 23:36:13 +0200

    useEffect with redux

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
@@ -5,6 +6,16 @@ import { useSelector } from 'react-redux';
 //#to utilize the state storeed in each slice
 function App() {
     const showCart = useSelector((state) => state.ui.cartIsVisible); //' the state of ui slice, identified by the ui because we can only have one reducer.
+    const cart = useSelector((state) => state.cart); //'the lastest cart
+
+    useEffect(() => {
+        fetch('https://react-c749e-default-rtdb.firebaseio.com/cart.json', {
+            method: 'PUT',
+            content: JSON.stringify(cart),
+        });
+
+    }, [cart]);// 'hence whenever the cart content changers, it the function will be rerendered
+
     return (
         <Layout>
             {showCart && <Cart />}
